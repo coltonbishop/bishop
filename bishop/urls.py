20f4d95baee26cb41e9f5fe671271ace5aa88037
Colton-Bishop <cmbishop@princeton.edu>
2019-06-21 09:38:50 -0400

    Resume Download Button

--- bishop/urls.py
+++ bishop/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.landing),
+	path('resume/', views.resume),
  	path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls),
 ]
--- bishop/views.py
+++ bishop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.static import serve
+import os
 # Create your views here.
 
 def landing(request):
 	context = {
 	}
 	return render(request, 'landing/index.html', context)
+
+def resume(request):
+	objectpath = 'projects/static/resume.pdf'
+	return serve(request, os.path.basename(objectpath), os.path.dirname(objectpath))
