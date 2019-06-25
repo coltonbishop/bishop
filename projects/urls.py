e15ccbdccc297e9b24edb2d26d6fc027a8c0a6b4
Colton-Bishop <cmbishop@princeton.edu>
2019-06-25 14:02:23 -0400

    Project Index Templates; Navigation

--- projects/urls.py
+++ projects/urls.py
@@ -5,18 +5,18 @@ from . import views
 app_name = 'language'
 urlpatterns = [
     path('', views.language, name='projects'),
-    path('deepmesh', views.language, name='language'),
-    path('AlquilandoSuite', views.language, name='language'),
-    path('HiveScience', views.language, name='language'),
-    path('Rype', views.language, name='language'),
-    path('symcnn', views.language, name='language'),
-    path('Wengga', views.language, name='language'),
-    path('WenggaNet', views.language, name='language'),
-    path('Donatello', views.language, name='language'),
-    path('Personal', views.language, name='language'),
-    path('Esg', views.language, name='language'),
-    path('Tigertales', views.language, name='language'),
-    path('Puentes', views.language, name='language'),
+    path('deepmesh', views.deepmesh, name='language'),
+    path('AlquilandoSuite', views.alquilando, name='language'),
+    path('HiveScience', views.hive, name='language'),
+    path('Rype', views.rype, name='language'),
+    path('symcnn', views.symcnn, name='language'),
+    path('Wengga', views.wengga, name='language'),
+    path('WenggaNet', views.wengganet, name='language'),
+    path('Donatello', views.donatello, name='language'),
+    path('Personal', views.personal, name='language'),
+    path('Esg', views.esg, name='language'),
+    path('Tigertales', views.tigertales, name='language'),
+    path('Puentes', views.puentes, name='language'),
 ]
 
 
--- projects/views.py
+++ projects/views.py
@@ -9,3 +9,63 @@ def language(request):
 	context = {
 	}
 	return render(request, 'gallery/index.html', context)
+
+def wengga(request):
+	context = {
+	}
+	return render(request, 'gallery/wengga.html', context)
+
+def deepmesh(request):
+	context = {
+	}
+	return render(request, 'gallery/deepmesh.html', context)
+
+def alquilando(request):
+	context = {
+	}
+	return render(request, 'gallery/alquilando.html', context)
+
+def hive(request):
+	context = {
+	}
+	return render(request, 'gallery/hive.html', context)
+
+def rype(request):
+	context = {
+	}
+	return render(request, 'gallery/rype.html', context)
+
+def symcnn(request):
+	context = {
+	}
+	return render(request, 'gallery/symcnn.html', context)
+
+def wengganet(request):
+	context = {
+	}
+	return render(request, 'gallery/wengganet.html', context)
+
+def donatello(request):
+	context = {
+	}
+	return render(request, 'gallery/donatello.html', context)
+
+def personal(request):
+	context = {
+	}
+	return render(request, 'gallery/personal.html', context)
+
+def esg(request):
+	context = {
+	}
+	return render(request, 'gallery/esg.html', context)
+
+def tigertales(request):
+	context = {
+	}
+	return render(request, 'gallery/tigertales.html', context)
+
+def puentes(request):
+	context = {
+	}
+	return render(request, 'gallery/puentes.html', context)
