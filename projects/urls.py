546fb30ebd30815b84cda50b473f366de36384dd
Colton-Bishop <cmbishop@princeton.edu>
2019-06-25 11:52:22 -0400

    Navigation to Project Index Pages

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,20 @@ from . import views
 
 app_name = 'language'
 urlpatterns = [
-    path('', views.language, name='language'),
+    path('', views.language, name='projects'),
+    path('deepmesh', views.language, name='language'),
+    path('AlquilandoSuite', views.language, name='language'),
+    path('HiveScience', views.language, name='language'),
+    path('Rype', views.language, name='language'),
+    path('symcnn', views.language, name='language'),
+    path('Wengga', views.language, name='language'),
+    path('WenggaNet', views.language, name='language'),
+    path('Donatello', views.language, name='language'),
+    path('Personal', views.language, name='language'),
+    path('Esg', views.language, name='language'),
+    path('Tigertales', views.language, name='language'),
+    path('Puentes', views.language, name='language'),
 ]
+
+
+
